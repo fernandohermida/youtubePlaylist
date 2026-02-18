d163e65e7348ced370cdc3cf25e6bf3f16d9f0c3
fernandohermida <fernandohermida@gmail.com>
2026-02-18 16:44:39 +0000

    update

--- src/types/index.ts
+++ src/types/index.ts
@@ -80,7 +80,10 @@ export interface YouTubePlaylistListItem {
   id: string;
   snippet: {
     thumbnails: {
-      medium?: { url: string; width: number; height: number };
+      default?: { url: string; width: number; height: number };
+      medium?:  { url: string; width: number; height: number };
+      high?:    { url: string; width: number; height: number };
+      maxres?:  { url: string; width: number; height: number };
     };
   };
   contentDetails: {
